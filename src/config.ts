6b89048b3f5fe2020831ab8746cef783525c4281
newton571 <ionorausky@icloud.com>
2026-09-17 19:32:07 -0700

    Update config.ts to my address

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "127.0.0.1",
-      port: 1111,
+      host: "Isaac_Newton54.aternos.me",
+      port: 36420,
     },
     tls: undefined,
   },
